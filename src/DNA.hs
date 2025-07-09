e64683a64c87edc99fb8a970aaa9324ed57ecfa2
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-09 14:59:23 +0200

    solution

--- src/DNA.hs
+++ src/DNA.hs
@@ -1,4 +1,13 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA xs = loop xs []
+  where
+    loop :: String -> String -> Either Char String
+    loop s acc
+      | null s = Right (reverse acc)
+      | head s == 'C' = loop (tail s) ('G' : acc)
+      | head s == 'G' = loop (tail s) ('C' : acc)
+      | head s == 'T' = loop (tail s) ('A' : acc)
+      | head s == 'A' = loop (tail s) ('U' : acc)
+      | otherwise = Left (head s)
