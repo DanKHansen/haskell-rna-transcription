5f09a27b9be8188605b9009f2de91910436b19d0
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-09 15:14:21 +0200

    refactor

--- src/DNA.hs
+++ src/DNA.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE LambdaCase #-}
+
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs = loop xs []
-  where
-    loop :: String -> String -> Either Char String
-    loop s acc
-      | null s = Right (reverse acc)
-      | head s == 'C' = loop (tail s) ('G' : acc)
-      | head s == 'G' = loop (tail s) ('C' : acc)
-      | head s == 'T' = loop (tail s) ('A' : acc)
-      | head s == 'A' = loop (tail s) ('U' : acc)
-      | otherwise = Left (head s)
+toRNA =
+  traverse
+    ( \case
+        'C' -> Right 'G'
+        'G' -> Right 'C'
+        'T' -> Right 'A'
+        'A' -> Right 'U'
+        c -> Left c
+    )
